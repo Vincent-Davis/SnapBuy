be9a3e6d338e819da6fc85045080856ff05d8a80
Vincent-Davis <leonardvincent62@gmail.com>
2024-09-09 13:28:00 +0700

    update views.py

--- main/views.py
+++ main/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'nama' : 'Royale Pass',
-        'price': 80000,
-        'description': 'CR GG EZ WIN, Overlevel everyone!',
-        'produk_terjual' : 0,
-        'rating' : 0.01,
+        'nama' : 'Vincent Davis Leonard Tjoeng',
+        'kelas': 'F',
+        'aplikasi' : 'SnapBuy'
     }
 
     return render(request, "main.html", context)
